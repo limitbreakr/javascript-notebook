f801c9f63cc4a6fa4217ca4ae89a8b3fc81e8b81
limitbreakr <limitbreakring@gmail.com>
2016-08-22 12:12:50 -0400

    Create script.js
    This contains some notes from the first 18 exercises of Codecademy's Javascript module.

---
+++ script.js
@@ -0,0 +1,10 @@
+// This is an example of a comment that the computer will not read
+// but I will!
+
+if (20 / 5 === "Four".length) {
+    console.log("Will print out because true.");
+} else {
+    confirm("A popup will appear because of false");
+    prompt("A question popup will enable the user to enter something.");
+    console.log("However, I don't yet know how to use that input for anything.");
+}
