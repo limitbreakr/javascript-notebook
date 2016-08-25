334746cb8cf4a751f7b6f945dc71a031d47012bc
limitbreakr <limitbreakring@gmail.com>
2016-08-25 17:50:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,26 @@
+// this is from the beginning
+
+console.log("A rectangle with length of 4 and width of 5 has a perimeter of " + rectPerimeter(4,5));
+// This is an example of a comment that the computer will not read
+// but I will!
+
+if (20 / 5 === "Four".length) {
+    console.log("Will print out because true.");
+} else {
+    confirm("A popup will appear because of false");
+    prompt("A question popup will enable the user to enter something.");
+    console.log("However, I don't yet know how to use that input for anything.");
+}
+// substring, positions, and a function
+
+var food = "Hamburgers";
+console.log(food.substring(3,10)); // will only say 'burgers'
+
+var rectPerimeter = function (length, width){
+    return 2*(length+width);
+};
+
+
 // aug25, adding some loops
 
 var cities = ["Toronto","Montreal","Ottawa","Vancouver","Winnipeg","Calgary"]
@@ -20,7 +43,6 @@ do {
 // switch statement
 
 var calling = prompt("What would you like your name to be?","Type any name you want here");
-
 switch(calling){
   case 'Terry':
     console.log("Are you Scary Terry?!");
@@ -38,25 +60,15 @@ switch(calling){
     console.log("I can't call you " + calling + " because it's dumb. You get to be called Toby.");
 }
 
-// substring, positions, and a function
+// objects
+var me = new Object();
+me["name"] = "Limit";
+me.age = 27;
+console.log("My name is " + me.name + " and I am " + me.age + " years old.");
 
-var food = "Hamburgers";
-console.log(food.substring(3,10)); // will only say 'burgers'
-
-var rectPerimeter = function (length, width){
-    return 2*(length+width);
+// would be the same as
+var meAlso = {
+    name: "Limit",
+    age: 27
 };
-
-// this is from the beginning
-
-console.log("A rectangle with length of 4 and width of 5 has a perimeter of " + rectPerimeter(4,5));
-// This is an example of a comment that the computer will not read
-// but I will!
-
-if (20 / 5 === "Four".length) {
-    console.log("Will print out because true.");
-} else {
-    confirm("A popup will appear because of false");
-    prompt("A question popup will enable the user to enter something.");
-    console.log("However, I don't yet know how to use that input for anything.");
-}
+console.log("My name is " + meAlso.name + " and I am " + me.Alsoage + " years old!.");
